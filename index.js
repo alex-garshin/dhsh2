5349badf9138fc213910087cc4c6f516ff16653f
Alex Garshin <gooddiz1@yandex.ru>
2023-08-09 23:54:00 +0300

    add scroll def

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const menu = document.querySelector('.header__menu');
-const buttonMenu = menu.querySelector('.header__menu-burger');
-const menuLinks = menu.querySelector('.header__menu-links');
-const menuLink = menu.querySelectorAll('.header__menu-link');
+const menu = document.querySelector(".header__menu");
+const buttonMenu = menu.querySelector(".header__menu-burger");
+const menuLinks = menu.querySelector(".header__menu-links");
+const menuLink = menu.querySelectorAll(".header__menu-link");
+const lupaSearch = menu.querySelector(".header__lupa");
 
-buttonMenu.addEventListener('click', () => {
-    switchMenu();
-  });
-  
-  const switchMenu = () => {
-    menuLinks.classList.toggle('menu__links_opened');
-  };
+buttonMenu.addEventListener("click", () => {
+  switchMenu();
+});
+
+const switchMenu = () => {
+  menuLinks.classList.toggle("menu__links_opened");
+};
+
+lupaSearch.addEventListener("click", () => {
+  searchMenu();
+});
+
+const searchMenu = () => {
+  menuLinks.classList.toggle("menu__links_opened");
+}
