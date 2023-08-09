e4697f7935a2b7adce4da00e010aaa6f7a06b567
Alex Garshin <gooddiz1@yandex.ru>
2023-08-10 01:13:15 +0300

    add full proj

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const buttonMenu = menu.querySelector(".header__menu-burger");
 const menuLinks = menu.querySelector(".header__menu-links");
 const menuLink = menu.querySelectorAll(".header__menu-link");
 const lupaSearch = menu.querySelector(".header__lupa");
+const searchBlock = menu.querySelector('.header__search-block')
+const lupaSearchBlack = menu.querySelector('.header__lupa-black')
 
 buttonMenu.addEventListener("click", () => {
   switchMenu();
@@ -17,5 +19,8 @@ lupaSearch.addEventListener("click", () => {
 });
 
 const searchMenu = () => {
-  menuLinks.classList.toggle("menu__links_opened");
+  menuLinks.classList.toggle("menu-links_closed");
+  searchBlock.classList.toggle("header__search-block_opened")
+  lupaSearchBlack.classList.toggle("header__search-block_opened")
+  lupaSearch.classList.toggle("header__lupa_closed")
 }
